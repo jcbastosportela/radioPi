ccd9d7c3446cc67e57ede15c815eb1430c2079f6
jcbastosportela <jcbastosportela@gmail.com>
2016-07-03 10:27:54 +0300

    added the first files

---
+++ IRadio.py
@@ -0,0 +1,149 @@
+from __future__ import unicode_literals
+import youtube_dl
+import logging
+import os
+import signal
+import sys
+import subprocess
+import parse
+
+SEPARATOR = chr(29)     # GS
+defRadioCMD = "omxplayer"
+
+#definition of possible sources
+SRC_YOUTUBE = "youtube"
+SRC_RADIO = "radio"
+
+#definition of possible commands
+CMD_PLAY = "play"
+CMD_VOLUP = "volup"
+CMD_VOLDOWN = "voldown"
+
+#definition of omxplayer commands
+OMX_PLAY = "p"
+OMX_VOLUP = "+"
+OMX_VOLDOWN = "-"
+
+class MyLogger(object):
+    def debug(self, msg):
+        pass
+
+    def warning(self, msg):
+        pass
+
+    def error(self, msg):
+        print(msg)
+
+
+def my_hook(d):
+    """
+
+    :param d: Dict with messages
+    :return: nothing
+    """
+    if d['status'] == 'finished':
+        print('Done downloading, now converting ...')
+
+
+def get_video_url(link):
+    """
+
+    :param link: The link to parse URL
+    :type link: unicode
+
+    :return: the parsed URL for the video
+    :rtype: str
+    """
+    ydl_opts = {
+        'format': 'bestaudio/best',
+        'postprocessors': [{
+            'key': 'FFmpegExtractAudio',
+            'preferredcodec': 'mp3',
+            'preferredquality': '192',
+        }],
+        'logger': MyLogger(),
+        'progress_hooks': [my_hook],
+    }
+    with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+        try:
+            result = ydl.extract_info(link, download=False)
+        except Exception:
+            return u''
+
+    if 'entries' in result:
+        # Can be a playlist or a list of videos
+        video = result['entries'][0]
+    else:
+        # Just a video
+        video = result
+
+    return "\"" + video.get('url') + "\""
+
+
+class IRadio:
+    def __init__(self):
+        self.log = logging.getLogger("IRadio")
+        streamH = logging.StreamHandler(sys.stderr)
+        self.log.setLevel(logging.DEBUG)
+        self.log.addHandler(streamH)
+        self.log.debug("log inited")
+        self.p = 0
+
+    def play(self, stream):
+        """
+        :param stream: The stream to open (URL or Path)
+        :type stream: str
+
+        :return nothing
+        :rtype: None
+        """
+        if self.p:
+            self.stop()
+        fullCMD = defRadioCMD + " " + stream
+        #call the radio app and get the stdin
+        self.p = subprocess.Popen(fullCMD, shell=True, stdin=subprocess.PIPE, preexec_fn=os.setsid)
+        self.log.info("Radio App started!")
+
+    def stop(self):
+        if self.p:
+            os.killpg(os.getpgid(self.p.pid), signal.SIGTERM)
+
+    def sendCmd(self, cmd):
+        self.p.stdin.write( cmd )
+        self.p.stdin.flush()
+        self.log.info("Command {0} sent to radio".format(cmd))
+
+    def process_command(self, cmd):
+        """
+        :param cmd: Frame
+        :type cmd: str
+
+        :return nothing
+        :rtype: None
+        """
+        #try:
+        ret = parse.search("src={:w}"+SEPARATOR, cmd)
+        if ret is not None and len(ret.fixed) != 0:
+            if SRC_YOUTUBE.lower() == ret.fixed[0].lower():
+                ret = parse.search("link={}" + SEPARATOR, cmd)
+                self.play(get_video_url(ret.fixed[0]))
+            elif SRC_RADIO.lower() == ret.fixed[0].lower():
+                ret = parse.search("url={}" + SEPARATOR, cmd)
+                if ret is not None and len(ret.fixed) != 0:
+                    self.play(ret.fixed[0])
+                    return
+                ret = parse.search("pls={}" + SEPARATOR, cmd)
+                if ret is not None and len(ret.fixed) != 0:
+                    # TODO Download the link of the playlist and parse the contents
+                    return
+            return
+
+        ret = parse.search("cmd={:w}" + SEPARATOR, cmd)
+        if ret is not None and len(ret.fixed) != 0:
+            if CMD_PLAY.lower() == ret.fixed[0].lower():
+                self.sendCmd(OMX_PLAY)
+
+
+        #except Exception:
+        #    self.log.error("ERR: Couldn't process message")
+        #    pass
---
+++ rad.py
@@ -0,0 +1,121 @@
+#!/usr/bin/python
+import time
+import logging
+import IRadio
+import socket
+import thread
+
+
+#http://www.radyo7.com/dinle/listen.php?ext=pls
+#http://7509.live.streamtheworld.com:443/METRO_FM_SC
+# init stuffs
+log = logging.getLogger("Main")
+RAD_CMD = "omxplayer"
+myRadio = IRadio.IRadio()
+sock = socket.socket()
+HOST = ""
+PORT = 5555
+BUFF = 1024
+FRAME_HEAD = chr(0x02)  #STX
+FRAME_TAIL = chr(0x03)  #ETX
+TCP_TIMEOUT = 20
+
+
+def get_tcp_cmds_handler(clientsock):
+    """
+
+    :param clientsock: The socket to listen
+    :type clientsock: socket._socketobject
+
+    :return: nothing
+    """
+    clientsock.setblocking(0)
+    frame = ""
+    log.info("Waiting messages from TCP...\n")
+    while 1:
+        log.debug("waiting new frame...\n")
+        frame = ""
+        b_frame = False
+        begin = time.time()
+        while not b_frame:
+            if frame != "" and time.time() - begin > TCP_TIMEOUT * 2:
+                log.info("TimeOut and some data " + frame )
+                frame = ""
+            try:
+                data = clientsock.recv(1)
+                if data:
+                    log.debug("got a bit " + data)
+                    frame += data
+                    log.debug("the frame is now " + frame)
+                    if frame.startswith(FRAME_HEAD) and frame.endswith(FRAME_TAIL):
+                        log.debug("frame COMPLETE")
+                        b_frame = True
+                    elif not frame.startswith(FRAME_HEAD):
+                        log.debug("INVALID head")
+                        if len(frame) >= len(FRAME_HEAD):
+                            log.debug("... longer than head")
+                            if FRAME_HEAD not in frame:
+                                log.debug("... No FRAME_HEAD found")
+                                frame = ""
+                            else:
+                                trash, value = frame.split(FRAME_HEAD, 1)
+                                frame = FRAME_HEAD + value
+                                log.debug("... FRAME_HEAD found " + frame)
+                    # change the beginning time for measurement
+                    begin = time.time()
+                else:
+                    # sleep for sometime to indicate a gap
+                    time.sleep(0.1)
+            except:
+                pass
+        log.info("Going to process " + frame)
+        myRadio.process_command(frame)
+        #v_url = get_video_url(frame)
+        #if v_url != u'':
+        #    myRadio.stop()
+        #    myRadio = IRadio.IRadio(v_url)
+        #if cmd != "\n" and cmd != "\r":
+            #radio.sendCmd(cmd)
+
+
+def __init__():
+    import sys
+    streamH = logging.StreamHandler(sys.stdout)
+    log.addHandler(streamH)
+    log.setLevel(logging.DEBUG)
+
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    log.info("Socket created")
+    sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    # Bind socket to local host and port
+    try:
+        sock.bind((HOST, PORT))
+    except socket.error as msg:
+        log.error("Bind failed. Error Code : {0} Message {1}".format(msg[0], msg[1]))
+        sys.exit()
+
+    log.info("Socket bind complete")
+
+    # Start listening on socket
+    sock.listen(10)
+    log.info("Socket now listening")
+
+    #myRadio = IRadio.IRadio(get_video_url("https://www.youtube.com/watch?v=hn3wJ1_1Zsg"))
+    myRadio.play("http://7509.live.streamtheworld.com:443/METRO_FM_SC")
+
+    # now keep talking with the client
+    while 1:
+        # wait to accept a connection - blocking call
+        conn, addr = sock.accept()
+        #log.info("Connected with {0} : {1}".format(addr[0], str(addr[1])))
+        thread.start_new_thread(get_tcp_cmds_handler, (conn,))
+
+    sock.close()
+
+
+# In case is called from terminal
+if __name__ == "__main__":
+    __init__()
+
+
+
