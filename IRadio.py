2cfa4129b86d551f1b63d948f41c62822613e440
jcbastosportela <jcbastosportela@gmail.com>
2016-07-10 12:57:46 +0200

    putting back OMX

--- IRadio.py
+++ IRadio.py
@@ -87,7 +87,7 @@ class IRadio:
         # youtube allways has to play with OMX
         self.player.stop()      # before creating new stop a potentially playing player
         #self.player = IPlayer.IPlayer(IPlayer.PLAYER_OMX)
-        self.player = IPlayer.IPlayer(IPlayer.PLAYER_MPLAYER)
+        self.player = IPlayer.IPlayer(IPlayer.PLAYER_OMX)
         url, title = get_video_url(link)
         self.player.play(url)
         self.display.setNowPlaying(title)
